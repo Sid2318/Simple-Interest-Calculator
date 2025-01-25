b7f3d83caed865330faba00af371a63796d20ddb
Sid2318 <moholsiddhi18@gmail.com>
2025-01-25 22:52:59 +0530

    25/01/2025

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+const [Principal , setprincipal] = useState(0)
+const [roi , setROI] = useState(0)
+const [tp, setTp] = useState(0)
+const [si, setSI] = useState(0)
+
+let cal = (e) => {
+  e.preventDefault()
+  let ans = (roi*Principal*tp)/100;
+  setSI(ans);
+}
+let reload = () => {
+  window.location.reload()
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple Interest Calculator</h1>
+      <p>Calculate your simple interest Easily</p>
+      <div>
+        <h1>₹{si}</h1>
+        <p>Simple Interest</p>
+      </div>
+      <form onSubmit = {cal}>
+        <input type = 'number' placeholder="Principal amonunt" onChange={(e) => setprincipal(e.target.value)}></input>
+        <input type = 'number' placeholder="Rate of interest (p.a)" onChange={(e) => setROI(e.target.value)}></input>
+        <input type = 'number' placeholder="Time period (yr)" onChange={(e) => setTp(e.target.value)}></input>
+        <div>
+          <button type = 'submit' >Submit</button>
+          <button onClick={reload}>Reload</button>
+        </div>
+      </form>
+
     </div>
   );
 }
